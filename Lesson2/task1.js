55dc0e62dd932e554b9efe1d5c3088d3228945f7
Serhii Kondratiuk <ss.allarma@gmail.com>
2026-05-01 17:09:45 +0300

    updating

---
+++ Lesson2/task1.js
@@ -0,0 +1,11 @@
+const String = "textString"
+const num = 5
+const isOk = true
+const isNull = null
+const undefiend1 = undefined
+
+console.log(String)
+console.log(num)
+console.log(isOk)
+console.log(isNull)
+console.log(undefiend1)
---
+++ Lesson2/task2.js
@@ -0,0 +1,6 @@
+const name1 = "Biba"
+const name2 = "Boba"
+
+let LoginGrats = `Hello ${name1}, Hello ${name2} , Welcome to the Moon!`
+
+console.log(LoginGrats)
